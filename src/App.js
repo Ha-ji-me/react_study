b480edbcde0cb3c4b8a876ab7adfd76436633a09
棚橋元 <hajime@tanahashimotonoMacBook-ea.local>
2021-04-01 15:36:25 +0900

    gggggg

--- src/App.js
+++ src/App.js
@@ -5,29 +5,39 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
+      counter:0, 
       msg:'count start!',
-      counter:0,
+      flg:true,
     }
     this.doAction = this.doAction.bind(this)
   }
 
-  doAction(event){
+  doAction(e){
     this.setState({
       counter: this.state.counter + 1,
-      msg: '*** count: ' + this.state.counter + ' *** '
+      msg: this.state.counter,
+      flg: !this.state.flg
     })
   }
-    
+
   render(){
     return  <div>
       <h1 className="bg-primary text-white display-4">React</h1>  
       <div className="container">
-        <p className="subtitle">Count number.</p>
-        <div className="alert alert-primary text-center">
-          <p className="h5 mb-4">{this.state.msg}</p>
-          <button className="btn btn-primary"
-          onClick={this.doAction}>
-          Click</button>
+        <p className="subtitle">Count number</p>
+        {this.state.flg ? 
+          <div className="alert alert-primary text-right">
+            <p className="h5">count: {this.state.msg}</p>
+          </div>
+        :
+          <div className="alert alert-warning  text-left">
+            <p className="h5">{this.state.msg}です。</p>
+          </div>
+        }
+        <div className="text-center">
+          <button className="btn btn-primary" 
+            onClick={this.doAction}>
+            Click</button>
         </div>
       </div>
     </div>
