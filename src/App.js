2f897d21978ba9c5c2c30928f1f40636590cfabe
棚橋元 <hajime@tanahashimotonoMacBook-ea.local>
2021-04-04 14:33:34 +0900

    2ho

--- src/App.js
+++ src/App.js
@@ -15,7 +15,12 @@ function CardMessage(props) {
 }
 
 function App() {
-  const [msg] = useState("this is sample message!")
+  const [msg, setMsg] = useState("this is sample message!")
+
+  const doAction = ()=>{
+    let res = window.prompt('type your name:')
+    setMsg("Hello, " + res + "!!")
+  }
 
   return (
     <div>
@@ -24,6 +29,11 @@ function App() {
         <h4 className="my-3">Hooks sample</h4>
         <AlertMessage message={msg} />
         <CardMessage message={msg} />
+        <div className="text-center">
+          <button onClick={doAction} className="btn btn-primary">
+            Click me!
+          </button>
+        </div>
       </div>
     </div>
   )
