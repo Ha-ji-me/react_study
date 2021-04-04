13f919f1d9e72555407adc101b8cf147be9cec84
棚橋元 <hajime@tanahashimotonoMacBook-ea.local>
2021-04-04 14:12:39 +0900

    2h

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import React, { useState } from 'react'
 import './App.css'
 
 
-function AlertMessage() {
+function AlertMessage(props) {
   return <div className="alert alert-primary h5 text-primary">
-    This is Alrt message!
+    {props.message}
   </div>
 }
-function CardMessage() {
+
+function CardMessage(props) {
   return <div className="card p-3 h5 border-primary text-center">
-    This is Card Message!
+    {props.message}
   </div>
 }
 
 function App() {
+  const [msg] = useState("this is sample message!")
+
   return (
     <div>
       <h1 className="bg-primary text-white display-4 ">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
-        <AlertMessage />
-        <CardMessage />
+        <AlertMessage message={msg} />
+        <CardMessage message={msg} />
       </div>
     </div>
   )
